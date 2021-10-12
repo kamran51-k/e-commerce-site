512f6cafd96fcd8dad57f894d72bd0277bd79609
kamran51-k <kamranasim0@gmail.com>
2021-10-12 20:44:48 +0400

    new html

--- settings/models.py
+++ settings/models.py
@@ -1,21 +1,26 @@
-from functools import cached_property
 from django.db import models
-from django.db.models.aggregates import Max
-from django.db.models.deletion import CASCADE
-from django.db.models.fields import BLANK_CHOICE_DASH, BooleanField, CharField, IntegerField
-from django.db.models.fields.files import ImageField
+
+
 # Create your models here.
 class NavbarModel(models.Model):
-    title = CharField(max_length=100,null=True,blank=True)
-    category = BooleanField()
+    title = models.CharField(max_length=100, null=True, blank=True)
+    category = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.title}'
+
 
 class CategoryModel(models.Model):
     category_id = models.ManyToManyField('NavbarModel')
-    style = CharField(max_length=100,null=True,blank=True)
-    
+    style = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.style}'
+
+
 class Products(models.Model):
-    name = CharField(max_length=100,null=True,blank=True)
-    
+    name = models.CharField(max_length=100, null=True, blank=True)
+
 
 class ImageModel(models.Model):
-    background_image = models.ImageField(upload_to='image',null=True,blank=True)
+    background_image = models.ImageField(upload_to='image', null=True, blank=True)
